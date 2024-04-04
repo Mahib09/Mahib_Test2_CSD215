257bd43400b2c361f8352c106bc36271c308f696
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-04-04 13:50:32 -0400

    Records and DU Defined and Movies list added

--- Program.fs
+++ Program.fs
@@ -1,2 +1,47 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Define the discriminated union for Genre
+type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+// Define the Director record type
+type Director = {
+    Name: string
+    Movies: int
+}
+
+// Define the Movie record type
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
+
+// Define movie instances based on the provided table
+let movies = [
+    { Name = "CODA"; RunLength = 111; Genre = Drama; Director = { Name = "Sian Heder"; Movies = 9 }; IMDBRating = 8.1 }
+    { Name = "Belfast"; RunLength = 98; Genre = Comedy; Director = { Name = "Kenneth Branagh"; Movies = 23 }; IMDBRating = 7.3 }
+    { Name = "Don't Look Up"; RunLength = 138; Genre = Comedy; Director = { Name = "Adam McKay"; Movies = 27 }; IMDBRating = 7.2 }
+    { Name = "Drive My Car"; RunLength = 179; Genre = Drama; Director = { Name = "Ryusuke Hamaguchi"; Movies = 16 }; IMDBRating = 7.6 }
+    { Name = "Dune"; RunLength = 155; Genre = Fantasy; Director = { Name = "Denis Villeneuve"; Movies = 24 }; IMDBRating = 8.1 }
+    { Name = "King Richard"; RunLength = 144; Genre = Sport; Director = { Name = "Reinaldo Marcus Green"; Movies = 15 }; IMDBRating = 7.5 }
+    { Name = "Licorice Pizza"; RunLength = 133; Genre = Comedy; Director = { Name = "Paul Thomas Anderson"; Movies = 49 }; IMDBRating = 7.4 }
+    { Name = "Nightmare Alley"; RunLength = 150; Genre = Thriller; Director = { Name = "Guillermo Del Toro"; Movies = 22 }; IMDBRating = 7.1 }
+]
+
+// Print all movies with run length in minutes
+printfn "All Movies:"
+movies 
+|> List.iter (fun movie -> printfn "Movie Name: %s | Run Length: %d minutes | Genre: %A | Director: %s | IMDB Rating: %.1f" 
+                                    movie.Name 
+                                    movie.RunLength 
+                                    movie.Genre 
+                                    movie.Director.Name 
+                                    movie.IMDBRating)
+
+
